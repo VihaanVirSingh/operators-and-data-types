efab3b032a66e7200d26f5aac41ee890ae312298
VihaanVirSingh <ishaanvir2014@gmail.com>
2026-04-18 10:55:42 +0530

    data types

---
+++ A1.py
@@ -0,0 +1,34 @@
+name = "Vihaan"
+age = 16
+Student = True
+Weight = 65
+
+#name
+print("Name is: ", name)
+print("Data type is: ", type(name))
+
+#Age
+print("age: ", age)
+print("Data type is: ", type(age))
+
+#Student
+print("is student: ", Student)
+print("Data Type is: ", type(Student))
+
+#Weight
+print("weight is: ", Weight)
+print("Data type is: ", type(Weight))
+
+print("")
+print("                            After type casting")
+print("")
+
+#age
+age1 = str(age)
+print("Age: ", age1)
+print("Data type age is: ", type(age1))
+
+#weight
+weight1 = float(Weight)
+print("weight is: ", weight1)
+print("Type of weight is: ", type(weight1))
